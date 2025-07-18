e3320d8239da28ef85a2d2fc199399f3ec386a09
BossReungwit <singlaboss@gmail.com>
2025-07-18 11:10:16 +0700

    add index navbar
    and css

--- webdoc/backend/urls.py
+++ webdoc/backend/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('manage/', views.manage_doc, name='manage_doc'),
     path('about/', views.about, name='about'),
     path('logout/', views.logout_view, name='logout'),
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
 ]

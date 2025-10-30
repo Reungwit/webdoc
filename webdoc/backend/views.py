cd680fdacb17f669835d193503af457f36218717
BossReungwit <singlaboss@gmail.com>
2025-10-30 10:46:26 +0700

    edit uri

--- webdoc/backend/views.py
+++ webdoc/backend/views.py
@@ -64,7 +64,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('webdoc:index')
+            return redirect('index')
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
